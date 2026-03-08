0b8bcc5b112bed1c5b714ffdca6cc25ed85f4800
davidyakoub <david.yakoub2008@gmail.com>
2026-03-07 21:54:04 -0800

    Godot Minigame 2

--- week-5-demo/scripts/toy.gd
+++ week-5-demo/scripts/toy.gd
@@ -16,7 +16,7 @@ func _on_body_entered(_body: Node) -> void:
 	
 	# STEP 1
 	# Write Step 1 right below this comment. 
-	
+	print(_bounces)
 	
 	%UI._updateBounces(_bounces)
 	
@@ -25,24 +25,27 @@ func _on_body_entered(_body: Node) -> void:
 	var b = _sprite.modulate.b
 	
 	# Step 4
-	
+	r = r + 0.1
 	# Step 5
-	
+	g = g - 0.1
 	# Step 7
-	
+	b *= 1.2
 	# Step 8
-		
+	if r > 1.0:
+		r = 0.0
 	# Step 9
-		
+	if g > 1.0:
+		g = 0.0
 	# Step 10
-	
+	if b > 1.0:
+		b = 0.0
 	_sprite.modulate = Color(r, g, b)
 	
 	# Step 6
-	
+	print(_sprite.modulate)
 	
 	# Step 11
+	var brightness = (r + g + b)/3
 	
-	
-	#print(brightness)
-	#%UI._updateBrightness(brightness)
+	print(brightness)
+	%UI._updateBrightness(brightness)
